66bb2cd7c40f141985c93e260495019aad9d386e
Evan Gordon <64689267+evangordo@users.noreply.github.com>
2021-03-10 12:26:08 +0000

    Add files via upload

---
+++ student05.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+class Student:
+	''' a class to represent a student'''
+	def __init__(self, name, exam, ca):
+		self.name = name
+		self.exam = exam_score
+		self.ca = ca_mark
+
+	def get_average(self):
+	  return float(self.exam + self.ca)/2
+
+	def __str__(self):
+		return "Name: " + self.name + ", Average: " + str(self.get_average())
+
+if __name__ == '__main__':
+	with open("students.txt", 'r') as f:
+	  for student in f:
+		student_deets = student.split(",")
+		name = student_deets[0]
+		exam_score = int(student_deets[1])
+		ca_mark = int(student_deets[2])
+		student = Student(name, exam_score, ca_mark)
+		print student.__str__()
